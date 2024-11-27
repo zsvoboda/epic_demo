9e6070892942124b8704a7d293b6108a905fc9f2
zsvoboda <zsvoboda@gmail.com>
2024-11-27 06:43:21 +0100

    Fixes

--- bin/fa/fa_setup.sh
+++ bin/fa/fa_setup.sh
@@ -17,21 +17,17 @@ pureds local user delete zsvoboda
 pureds local group delete epic_daemons
 
 # Detach and remove the NFS export policy 
-echo "Removing NFS export policy"
 purepolicy nfs remove --dir fs_epic:md_epic_exchange p_exchange_dir_nfs
 purepolicy nfs delete p_exchange_dir_nfs
 
 # Detach and remove the SMB share policy
-echo "Removing SMB share policy"
-purepolicy smb remove --dir fs_epic:md_epic_exchange p_exchange_dir_nfs
-purepolicy smb delete p_exchange_dir_nfs
+purepolicy smb remove --dir fs_epic:md_epic_exchange p_exchange_dir_smb
+purepolicy smb delete p_exchange_dir_smb
 
 # Delete the managed directory
-echo "Deleting managed directory"
 puredir delete fs_epic:md_epic_exchange
 
 # Delete and eradicate the filesystem
-echo "Deleting and eradicating filesystem"
 purefs destroy fs_epic
 purefs eradicate fs_epic
 
@@ -58,29 +54,23 @@ pureds local group create --gid 2100 epic_daemons
 { echo 'password'; echo 'password'; } | pureds local user create --password --uid 502 --primary-group Administrators zsvoboda
 
 # Filesystem
-echo "Creating filesystem"
 purefs create fs_epic
 
 # Managed directory
-echo "Creating managed directory"
 puredir create --path /home fs_epic:md_epic_exchange
 
 # NFS export policy
-echo "Creating NFS export policy"
 purepolicy nfs create p_exchange_dir_nfs
 purepolicy nfs rule add --client "*" --all-squash --anonuid 2101 --anongid 2100 --version nfsv3 p_exchange_dir_nfs
 
 # NFS export
-echo "Creating NFS export"
 purepolicy nfs add --dir fs_epic:md_epic_exchange --export-name EXCHANGE p_exchange_dir_nfs
 
 # SMB share policy
-echo "Creating SMB share policy"
 purepolicy smb create p_exchange_dir_smb
 purepolicy smb rule add --client "*" --anonymous-access-allowed p_exchange_dir_smb
 
 # SMB share
-echo "Creating SMB share"
 purepolicy smb add --dir fs_epic:md_epic_exchange --export-name EXCHANGE p_exchange_dir_smb
 
 # restore stdout from fd3
