32405b16dd9c705aef80dc2b4f0906fcd9bdf462
zsvoboda <zsvoboda@gmail.com>
2024-11-27 11:01:54 +0100

    Fixes

---
+++ bin/demo_anonymous.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+sudo mkdir -p /mnt/epic_exchange_anonymous
+sudo chmod 777 /mnt/epic_exchange_anonymous
+sudo mount -t nfs -o vers=3 ${FA_MOUNT_IP}:/EXCHANGE_ANONYMOUS /mnt/epic_exchange_anonymous/
+
+if [ "$#" != "1" -o -z "$1" ]; then
+    echo "Usage: $0 import"
+    echo "       $0 export"
+    exit 1
+fi
+
+if [ "$1" = "import" ]; then
+    if [ -d /mnt/epic_exchange/import ]; then
+        for FILE in /mnt/epic_exchange_anonymous/import/*; do
+            if [ -f "$FILE" ]; then
+                echo "Importing: $FILE"
+                cat "$FILE"
+                echo -e "\n------------------------\n"
+            fi
+        done
+    else
+        echo "Import directory does not exist."
+    fi
+fi
+
+if [ "$1" = "export" ]; then
+    if [ -d /mnt/epic_exchange/export ]; then
+        echo "Demo CSV Content" > /mnt/epic_exchange_anonymous/export/export.csv
+    else
+        echo "Export directory does not exist."
+    fi
+fi
+
+sudo umount /mnt/epic_exchange_anonymous
