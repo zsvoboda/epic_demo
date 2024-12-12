d21df3d94c5e902bc26f6036dc3302e876017cb4
zsvoboda <zsvoboda@gmail.com>
2024-12-12 14:43:30 +0100

    PUREUSER

--- bin/env.sh
+++ bin/env.sh
@@ -4,6 +4,3 @@
 export FA_CONTROLLER_IP="10.0.0.11"
 # FA NFS mount IP (VIF, File Virtual Interface)
 export FA_MOUNT_IP="10.0.0.13"
-
-export FA_ADMIN_USER="pureuser"
-export FA_ADMIN_PASSWORD="pureuser"
